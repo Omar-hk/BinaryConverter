7b9b6803ae0d99dd91a3727d5e668db927568851
Omar-hk <63765054+Omar-hk@users.noreply.github.com>
2022-09-23 16:06:43 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+ 
+ 
+static String getBinaryString(int d)
+{
+    int[] arr = new int[20];
+    int i = 0;
+
+    StringBuilder binaryBuilder = new StringBuilder();
+
+    while (d > 0)
+    {
+        arr[i] = d % 2;
+        d = d / 2;
+        i++;
+
+    }
+    i--;
+    while (i >= 0)
+    {
+        binaryBuilder.Append(arr[i]);
+        i--;
+    }
+    return binaryBuilder.ToString();
+}
+
+int no;
+Console.WriteLine("enter the decimal number: ");
+no = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Binary " + getBinaryString(no));
+
